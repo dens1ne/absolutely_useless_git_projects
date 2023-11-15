5ebef276334d01fa223161021b5a37e254189d4f
densine <i@djantonjuk.ru>
2023-11-15 16:52:26 +0300

    asldghs;ladhglkjadh ;fgo;idvnf,kgnhsaldkvnhguiasdyvgkhdfjvh

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(__name__)
+def twice(func):
+    def new_func(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+
+    return new_func
+
+
+@twice
+def print_name(your_name: str) -> bool:
+    print('Hi,', your_name)
+    return True
+
+
+print_name('denis')
